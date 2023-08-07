b41267839f82c41d924017a3e687c7aacd45ede6
Barbramwendwa <j-16-1414-2022@student.mksu.ac.ke>
2023-08-07 07:04:30 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+ /**
+  * _isalpha checks for an alphabet
+  * returns 1 if it's a letter whether uppercase or lowercase
+  * returns 0 otherwise
+  * @c character is the one to check
+  *
+  */
+
+int _isalpha(int c)
+{
+        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+        {
+                return (1);
+        }
+        return (0);
+}
+
