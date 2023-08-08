e06b975ded59dc4d12b012891f96bc54cd55b036
Barbramwendwa <j16-1414-2022@student.mksu.ac.ke>
2023-08-08 08:11:15 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,13 +16,13 @@ int print_sign(int n)
                 return (1);
         }
        		else if (n < 0)
-	}
-                _putchar(45);
-                return (-1);
+	{
+                _putchar(...45);
+                (return (-1));
         } 	
-		else
+		(else)
         {
                 _putchar(48);
-                return(0);
+                (return(0));
 	}
 }
