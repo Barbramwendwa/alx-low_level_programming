ed3702743c77ee00cf881b2c45e7b0d1cff969c9
Barbramwendwa <j16-1414-2022@student.mksu.ac.ke>
2023-08-08 08:51:15 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,18 +14,19 @@ void jack_bauer(void)
 
 
         while (i < 24)
-        {
-                j = 0;
-                while (j < 60)
-                {
-                        _putchar((i / 10) + '0');
-                        _putchar((i % 10) + '0');
-                        _putchar(':');
-                        _putchar((j / 10) + '0');
-                        _putchar((j % 10) + '0');
-                        _putchar('\n');
-                        j++;
-                }
-                i++;
-        }
+     
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0')
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
 }
