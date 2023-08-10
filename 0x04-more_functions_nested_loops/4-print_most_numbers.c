011e0d32704d9ec03c68d1403828a784467bab5a
Barbramwendwa <j16-1414-2022@student.mksu.ac.ke>
2023-08-10 10:35:10 -0700

    Fixing betty documentation.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * -print_most_numbers - print 0 to 9 ommiting 2 and 4.
- *
- *  return void.
+ * print_most_numbers - print 0 to 9 ommiting 2 and 4.
+ * Return: void.
  */
 
 void print_most_numbers(void)
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		while (c != '2' && c != '4')
