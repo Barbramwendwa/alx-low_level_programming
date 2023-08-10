9603a81be2a81547815d41de3753720fdd31d4f9
Barbramwendwa <j16-1414-2022@student.mksu.ac.ke>
2023-08-10 11:03:40 -0700

    Hot fix.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 to 9 ommiting 2 and 4.
- * Return: void.
+ * print_most_numbers - prints numbers 0 to 9
+ * Return: 0
  */
 
 void print_most_numbers(void)
@@ -12,9 +12,7 @@ void print_most_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
-		{
 			_putchar(c);
-		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
